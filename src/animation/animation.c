9565ff44580ef1a1e8bb35a317edf4189b2fd385
Eignar17 <Eignar17@gmail.com>
2018-07-02 18:14:33 -0500

    Update animation.c

--- src/animation/animation.c
+++ src/animation/animation.c
@@ -867,6 +867,10 @@ expandBoxWithPoints3DTransform (CompOutput          *output,
     return TRUE;
 }
 
+
+/* ANIM: Needs exporting */
+/* model.c */
+
 static void
 modelUpdateBB (CompOutput *output,
 	       CompWindow * w,
@@ -1282,6 +1286,8 @@ animGetScreenOptions(CompPlugin *plugin, CompScreen * screen, int *count)
     return as->opt;
 }
 
+/* ANIM */
+/* model.c */
 static void
 objectInit(Object * object,
 	   float positionX, float positionY,
@@ -1503,6 +1509,8 @@ animEnsureModel(CompWindow * w)
 
     return TRUE;
 }
+
+/* ^^ model.c */
 
 static void cleanUpParentChildChainItem(AnimScreen *as, AnimWindow *aw)
 {
@@ -1819,6 +1827,7 @@ animActivateEvent (CompScreen *s,
     o[1].value.b = activating;
 
     (*s->display->handleCompizEvent) (s->display, "animation", "activate", o, 2);
+    }
 }
 
 static const PluginEventInfo watchedPlugins[] =
@@ -2071,7 +2080,7 @@ initiateFocusAnimation(CompWindow *w)
 		    adw->com.animRemainingTime = adw->com.animTotalTime;
 
 		    // Put window on dodge chain
-
+			
 		    // if dodge chain was started before
 		    if (wDodgeChainLastVisited)
 		    {
@@ -2164,6 +2173,7 @@ initiateFocusAnimation(CompWindow *w)
 }
 
 // returns whether this window is relevant for fade focus
+ // MOD?
 static Bool
 relevantForFadeFocus(CompWindow *nw)
 {
@@ -2182,69 +2192,8 @@ relevantForFadeFocus(CompWindow *nw)
     return isWinVisible(nw);
 }
 
-static Bool
-restackInfoStillGood(CompScreen *s, RestackInfo *restackInfo)
-{
-    Bool wStartGood = FALSE;
-    Bool wEndGood = FALSE;
-    Bool wOldAboveGood = FALSE;
-    Bool wRestackedGood = FALSE;
-
-    CompWindow *w;
-    for (w = s->windows; w; w = w->next)
-    {
-	if (restackInfo->wStart == w && isWinVisible(w))
-	    wStartGood = TRUE;
-	if (restackInfo->wEnd == w && isWinVisible(w))
-	    wEndGood = TRUE;
-	if (restackInfo->wRestacked == w && isWinVisible(w))
-	    wRestackedGood = TRUE;
-	if (restackInfo->wOldAbove == w && isWinVisible(w))
-	    wOldAboveGood = TRUE;
-    }
-    return (wStartGood && wEndGood && wOldAboveGood && wRestackedGood);
-}
-
-// Reset stacking related info
-static void
-resetStackingInfo (CompScreen *s)
-{
-    CompWindow *w;
-    for (w = s->windows; w; w = w->next)
-    {
-	ANIM_WINDOW (w);
-
-	aw->configureNotified = FALSE;
-	if (aw->restackInfo)
-	{
-	    free (aw->restackInfo);
-	    aw->restackInfo = NULL;
-	}
-    }
-}
-
-// Returns TRUE if linking wCur to wNext would not result
-// in a circular chain being formed.
-static Bool
-wontCreateCircularChain (CompWindow *wCur, CompWindow *wNext)
-{
-    ANIM_SCREEN (wCur->screen);
-    AnimWindow *awNext = NULL;
-
-    while (wNext)
-    {
-	if (wNext == wCur) // would form circular chain
-	    return FALSE;
-
-	awNext = GET_ANIM_WINDOW (wNext, as);
-	if (!awNext)
-	    return FALSE;
-
-	wNext = awNext->moreToBePaintedNext;
-    }
-    return TRUE; 
-}
-
+// MOD?
+	
 static void animPreparePaintScreen(CompScreen * s, int msSinceLastPaint)
 {
     CompWindow *w;
@@ -2268,130 +2217,8 @@ static void animPreparePaintScreen(CompScreen * s, int msSinceLastPaint)
 
     if (as->aWinWasRestackedJustNow)
     {
-	/*
-	  Handle focusing windows with multiple utility/dialog windows
-	  (like gobby), as in this case where gobby was raised with its
-	  utility windows:
-
-	  was: C0001B 36000A5 1E0000C 1E0005B 1E00050 3205B63 600003 
-	  now: C0001B 36000A5 1E0000C 1E00050 3205B63 1E0005B 600003 
-
-	  was: C0001B 36000A5 1E0000C 1E00050 3205B63 1E0005B 600003 
-	  now: C0001B 36000A5 1E0000C 3205B63 1E00050 1E0005B 600003 
-
-	  was: C0001B 36000A5 1E0000C 3205B63 1E00050 1E0005B 600003 
-	  now: C0001B 36000A5 3205B63 1E0000C 1E00050 1E0005B 600003 
-	*/
-	CompWindow *wOldAbove = NULL;
-	for (w = s->windows; w; w = w->next)
-	{
-	    ANIM_WINDOW(w);
-	    if (aw->restackInfo)
-	    {
-		if (aw->com.curWindowEvent != WindowEventNone ||
-		    otherPluginsActive(as) ||
-		    // Don't animate with stale restack info
-		    !restackInfoStillGood(s, aw->restackInfo))
-		{
-		    continue;
-		}
-		if (!wOldAbove)
-		{
-		    // Pick the old above of the bottommost one
-		    wOldAbove = aw->restackInfo->wOldAbove;
-		}
-		else
-		{
-		    // Use as wOldAbove for every focus fading window
-		    // (i.e. the utility/dialog windows of an app.)
-		    if (wOldAbove != w)
-			aw->restackInfo->wOldAbove = wOldAbove;
-		}
-	    }
-	}
-	// do in reverse order so that focus-fading chains are handled
-	// properly
-	for (w = s->reverseWindows; w; w = w->prev)
-	{
-	    ANIM_WINDOW(w);
-	    if (aw->restackInfo)
-	    {
-		if (aw->com.curWindowEvent != WindowEventNone ||
-		    // Don't initiate focus anim for current dodgers
-		    aw->com.curAnimEffect != AnimEffectNone ||
-		    // Don't initiate focus anim for windows being passed thru
-		    aw->winPassingThrough ||
-		    otherPluginsActive(as) ||
-		    // Don't animate with stale restack info
-		    !restackInfoStillGood(s, aw->restackInfo))
-		{
-		    free(aw->restackInfo);
-		    aw->restackInfo = NULL;
-		    continue;
-		}
-
-		// Find the first window at a higher stacking order than w
-		CompWindow *nw;
-		for (nw = w->next; nw; nw = nw->next)
-		{
-		    if (relevantForFadeFocus(nw))
-			break;
-		}
-
-		// If w is being lowered, there has to be a window
-		// at a higher stacking position than w (like a panel)
-		// which this w's copy can be painted before.
-		// Otherwise the animation will only show w fading in
-		// rather than 2 copies of it cross-fading.
-		if (!aw->restackInfo->raised && !nw)
-		{
-		    // Free unnecessary restackInfo
-		    free(aw->restackInfo);
-		    aw->restackInfo = NULL;
-		    continue;
-		}
-
-		// Check if above window is focus-fading too.
-		// (like a dialog of an app. window)
-		// If so, focus-fade this together with the one above
-		// (link to it)
-		if (nw)
-		{
-		    AnimWindow *awNext = GET_ANIM_WINDOW(nw, as);
-		    if (awNext && awNext->winThisIsPaintedBefore &&
-			wontCreateCircularChain (w, nw))
-		    {
-			awNext->moreToBePaintedPrev = w;
-			aw->moreToBePaintedNext = nw;
-			aw->restackInfo->wOldAbove =
-			    awNext->winThisIsPaintedBefore;
-		    }
-		}
-		initiateFocusAnimation(w);
-	    }
-	}
-
-	for (w = s->reverseWindows; w; w = w->prev)
-	{
-	    ANIM_WINDOW(w);
-
-	    if (!aw->isDodgeSubject)
-		continue;
-	    Bool dodgersAreOnlySubjects = TRUE;
-	    CompWindow *dw;
-	    AnimWindow *adw;
-	    for (dw = aw->dodgeChainStart; dw; dw = adw->dodgeChainNext)
-	    {
-		adw = GET_ANIM_WINDOW(dw, as);
-		if (!adw)
-		    break;
-		if (!adw->isDodgeSubject)
-		    dodgersAreOnlySubjects = FALSE;
-	    }
-	    if (dodgersAreOnlySubjects)
-		aw->skipPostPrepareScreen = TRUE;
-	}
-    }
+	handleWindowRestack (s);
+     }
 	
     if (as->animInProgress)
     {
@@ -3192,201 +3019,6 @@ animPaintWindow(CompWindow * w,
     return status;
 }
 
-// Go to the bottommost window in this "focus chain"
-// This chain is used to handle some cases: e.g when Find dialog
-// of an app is open, both windows should be faded when the Find
-// dialog is raised.
-static CompWindow*
-getBottommostInFocusChain (CompWindow *w)
-{
-    if (!w)
-	return w;
-
-    ANIM_WINDOW (w);
-    ANIM_SCREEN (w->screen);
-
-    CompWindow *bottommost = aw->winToBePaintedBeforeThis;
-
-    if (!bottommost || bottommost->destroyed)
-	return w;
-
-    AnimWindow *awBottommost = GET_ANIM_WINDOW (bottommost, as);
-    CompWindow *wPrev = NULL;
-
-    if (awBottommost)
-	wPrev = awBottommost->moreToBePaintedPrev;
-    while (wPrev)
-    {
-	bottommost = wPrev;
-	wPrev = GET_ANIM_WINDOW(wPrev, as)->moreToBePaintedPrev;
-    }
-    return bottommost;
-}
-
-static void
-resetWalkerMarks (CompScreen *s)
-{
-    CompWindow *w;
-    for (w = s->windows; w; w = w->next)
-    {
-	ANIM_WINDOW(w);
-	aw->walkerOverNewCopy = FALSE;
-	aw->walkerVisitCount = 0;
-    }
-}
-
-static CompWindow*
-animWalkFirst (CompScreen *s)
-{
-    ANIM_SCREEN (s);
-
-    resetWalkerMarks (s);
-
-    CompWindow *w = getBottommostInFocusChain(s->windows);
-    if (w)
-    {
-	AnimWindow *aw = GET_ANIM_WINDOW (w, as);
-	aw->walkerVisitCount++;
-    }
-    return w;
-}
-
-static CompWindow*
-animWalkLast (CompScreen *s)
-{
-    ANIM_SCREEN (s);
-
-    resetWalkerMarks (s);
-
-    CompWindow *w = s->reverseWindows;
-    if (w)
-    {
-	AnimWindow *aw = GET_ANIM_WINDOW (w, as);
-	aw->walkerVisitCount++;
-    }
-    return w;
-}
-
-static Bool
-markNewCopy (CompWindow *w)
-{
-    ANIM_WINDOW (w);
-
-    // if window is in a focus chain
-    if (aw->winThisIsPaintedBefore ||
-	aw->moreToBePaintedPrev)
-    {
-	aw->walkerOverNewCopy = TRUE;
-	return TRUE;
-    }
-    return FALSE;
-}
-
-static CompWindow*
-animWalkNext (CompWindow *w)
-{
-    ANIM_WINDOW (w);
-    CompWindow *wRet = NULL;
-
-    if (!aw->walkerOverNewCopy)
-    {
-	// Within a chain? (not the 1st or 2nd window)
-	if (aw->moreToBePaintedNext)
-	    wRet = aw->moreToBePaintedNext;
-	else if (aw->winThisIsPaintedBefore) // 2nd one in chain?
-	    wRet = aw->winThisIsPaintedBefore;
-    }
-    else
-	aw->walkerOverNewCopy = FALSE;
-
-    if (!wRet && w->next && markNewCopy (w->next))
-	wRet = w->next;
-    else if (!wRet)
-	wRet = getBottommostInFocusChain(w->next);
-
-    if (wRet)
-    {
-	ANIM_SCREEN (w->screen);
-
-	AnimWindow *awRet = GET_ANIM_WINDOW (wRet, as);
-	// Prevent cycles, which cause freezes
-	if (awRet->walkerVisitCount > 1) // each window is visited at most twice
-	    return NULL;
-	awRet->walkerVisitCount++;
-    }
-    return wRet;
-}
-
-static CompWindow*
-animWalkPrev (CompWindow *w)
-{
-    ANIM_WINDOW (w);
-    CompWindow *wRet = NULL;
-
-    // Focus chain start?
-    CompWindow *w2 = aw->winToBePaintedBeforeThis;
-    if (w2)
-	wRet = w2;
-    else if (!aw->walkerOverNewCopy)
-    {
-	// Within a focus chain? (not the last window)
-	CompWindow *wPrev = aw->moreToBePaintedPrev;
-	if (wPrev)
-	    wRet = wPrev;
-	else if (aw->winThisIsPaintedBefore) // Focus chain end?
-	    // go to the chain beginning and get the
-	    // prev. in X stacking order
-	{
-	    if (aw->winThisIsPaintedBefore->prev)
-		markNewCopy (aw->winThisIsPaintedBefore->prev);
-
-	    wRet = aw->winThisIsPaintedBefore->prev;
-	}
-    }
-    else
-	aw->walkerOverNewCopy = FALSE;
-
-    if (!wRet && w->prev)
-	markNewCopy (w->prev);
-
-    wRet = w->prev;
-    if (wRet)
-    {
-	ANIM_SCREEN (w->screen);
-
-	AnimWindow *awRet = GET_ANIM_WINDOW (wRet, as);
-	// Prevent cycles, which cause freezes
-	if (awRet->walkerVisitCount > 1) // each window is visited at most twice
-	    return NULL;
-	awRet->walkerVisitCount++;
-    }
-    return wRet;
-}
-
-static void
-animInitWindowWalker (CompScreen *s,
-		      CompWalker *walker)
-{
-    ANIM_SCREEN (s);
-
-    UNWRAP (as, s, initWindowWalker);
-    (*s->initWindowWalker) (s, walker);
-    WRAP (as, s, initWindowWalker, animInitWindowWalker);
-
-    if (as->walkerAnimCount > 0) // only walk if necessary
-    {
-	if (!as->animInProgress) // just in case
-	{
-	    as->walkerAnimCount = 0;
-	    return;
-	}
-	walker->first = animWalkFirst;
-	walker->last  = animWalkLast;
-	walker->next  = animWalkNext;
-	walker->prev  = animWalkPrev;
-    }
-}
-
 static void animHandleCompizEvent(CompDisplay * d, const char *pluginName,
 				  const char *eventName, CompOption * option,
 				  int nOption)
@@ -3862,144 +3494,8 @@ static void animHandleEvent(CompDisplay * d, XEvent * event)
 	// if restacking occurred and not window open/close
 	if (!winOpenedClosed)
 	{
-	    ANIM_WINDOW(w);
-	    aw->configureNotified = TRUE;
-
-	    // Find which window is restacked 
-	    // e.g. here 8507730 was raised:
-	    // 54526074 8507730 48234499 14680072 6291497
-	    // 54526074 48234499 14680072 8507730 6291497
-	    // compare first changed win. of row 1 with last
-	    // changed win. of row 2, and vica versa
-	    // the matching one is the restacked one
-	    CompWindow *wRestacked = 0;
-	    CompWindow *wStart = 0;
-	    CompWindow *wEnd = 0;
-	    CompWindow *wOldAbove = 0;
-	    CompWindow *wChangeStart = 0;
-	    CompWindow *wChangeEnd = 0;
-
-	    Bool raised = FALSE;
-	    int changeStart = -1;
-	    int changeEnd = -1;
-
-	    int i;
-	    for (i = 0; i < n; i++)
-	    {
-		CompWindow *wi =
-		    findWindowAtScreen (s, clientListStacking[i]);
-
-		// skip if minimized (prevents flashing problem)
-		if (!wi || !isWinVisible(wi))
-		    continue;
-
-		// skip if (tabbed and) hidden by Group plugin
-		if (wi->state & (CompWindowStateSkipPagerMask |
-				 CompWindowStateSkipTaskbarMask))
-		    continue;
-
-		if (clientListStacking[i] !=
-		    as->lastClientListStacking[i])
-		{
-		    if (changeStart < 0)
-		    {
-			changeStart = i;
-			wChangeStart = wi; // make use of already found w
-		    }
-		    else
-		    {
-			changeEnd = i;
-			wChangeEnd = wi;
-		    }
-		}
-		else if (changeStart >= 0) // found some change earlier
-		    break;
-	    }
-
-	    // if restacking occurred
-	    if (changeStart >= 0 && changeEnd >= 0)
-	    {
-		CompWindow *w2;
-
-		// if we have only 2 windows changed, 
-		// choose the one clicked on
-		Bool preferRaised = FALSE;
-		Bool onlyTwo = FALSE;
-
-		if (wChangeEnd &&
-		    clientListStacking[changeEnd] ==
-		    as->lastClientListStacking[changeStart] &&
-		    clientListStacking[changeStart] ==
-		    as->lastClientListStacking[changeEnd])
-		{
-		    // Check if the window coming on top was
-		    // configureNotified (clicked on)
-		    AnimWindow *aw2 = GET_ANIM_WINDOW(wChangeEnd, as);
-		    if (aw2->configureNotified)
-		    {
-			preferRaised = TRUE;
-		    }
-		    onlyTwo = TRUE;
-		}
-		// Clear all configureNotified's
-		for (w2 = s->windows; w2; w2 = w2->next)
-		{
-		    AnimWindow *aw2 = GET_ANIM_WINDOW(w2, as);
-		    aw2->configureNotified = FALSE;
-		}
-
-		if (preferRaised ||
-		    (!onlyTwo &&
-		     clientListStacking[changeEnd] ==
-		     as->lastClientListStacking[changeStart]))
-		{
-		    // raised
-		    raised = TRUE;
-		    wRestacked = wChangeEnd;
-		    wStart = wChangeStart;
-		    wEnd = wRestacked;
-		    wOldAbove = wStart;
-		}
-		else if (clientListStacking[changeStart] ==
-			 as->lastClientListStacking[changeEnd] && // lowered
-			 // We don't animate lowering if there is no
-			 // window above this window, since this window needs
-			 // to be drawn on such a "host" in animPaintWindow
-			 // (at least for now).
-			 changeEnd < n - 1)
-		{
-		    wRestacked = wChangeStart;
-		    wStart = wRestacked;
-		    wEnd = wChangeEnd;
-		    wOldAbove = findWindowAtScreen
-			(s, as->lastClientListStacking[changeEnd+1]);
-		}
-		for (; wOldAbove && !isWinVisible(wOldAbove);
-		     wOldAbove = wOldAbove->next)
-		    ;
-	    }
-	    if (wRestacked && wStart && wEnd && wOldAbove)
-	    {
-		AnimWindow *awRestacked = GET_ANIM_WINDOW(wRestacked, as);
-		if (awRestacked->created)
-		{
-		    RestackInfo *restackInfo = calloc(1, sizeof(RestackInfo));
-		    if (restackInfo)
-		    {
-			restackInfo->wRestacked = wRestacked;
-			restackInfo->wStart = wStart;
-			restackInfo->wEnd = wEnd;
-			restackInfo->wOldAbove = wOldAbove;
-			restackInfo->raised = raised;
-
-			if (awRestacked->restackInfo)
-			    free(awRestacked->restackInfo);
-
-			awRestacked->restackInfo = restackInfo;
-			as->aWinWasRestackedJustNow = TRUE;
-		    }
-		}
-	    }
+	    fprintf(stderr, "n is %i\n", n);
+	    saveRestackInfo (w, clientListStacking, n);
 	}
 	updateLastClientListStacking(s);
     }
@@ -4797,6 +4293,43 @@ AnimEffect AnimEffectZoom = &(AnimEffectInfo)
       .zoomToIconFunc		= returnTrue}};
 
 AnimEffect animEffects[NUM_EFFECTS];
+
+AnimEngine AnimEngineTransform = &(AnimEngine)
+    {"animation:engineTransform",
+     {.prePaintOutputFunc      = engTransformPrePaintOutput,
+      .postPaintOutputFunc     = engTransformPostPaintOutput,
+      .prePaintWindowFunc      = engTransformPrePaintWindow,
+      .postPaintWindowFunc     = engTransformPostPaintWindow
+     }
+    }
+
+AnimEngine AnimEngineAttrib = &(AnimEngine)
+    {"animation:engineAttrib",
+     {.prePaintOutputFunc      = engAttribPrePaintOutput,
+      .postPaintOutputFunc     = engAttribPostPaintOutput,
+      .prePaintWindowFunc      = engAttribPrePaintWindow,
+      .postPaintWindowFunc     = engAttribPostPaintWindow     
+    }
+
+AnimEngine AnimEngineTransform = &(AnimEngine)
+    {"animation:engineModel",
+     {.prePaintOutputFunc      = engModelPrePaintOutput,
+      .postPaintOutputFunc     = engModelPostPaintOutput,
+      .prePaintWindowFunc      = engModelPrePaintWindow,
+      .postPaintWindowFunc     = engModelPostPaintWindow,
+      .preHandleEventFunc      = engModelPreHandleEvent,
+      .postHandleEventFunc     = engModelPostHandleEvent,
+      .moveNotifyFunc          = engModelMoveNotify,
+      .resizeNotifyFunc        = engModelResizeNotify
+     }
+    }
+
+AnimEngine AnimEngineRestacker = &(AnimEngine
+    {"animation:engineRestacker",
+     {.preHandleEventFunc      = engRestackPreHandleEvent,
+      .postHandleEventFunc     = engRestackPostHandleEvent,
+
+AnimEngine animEgnines[NUM_ENGINES];
 
 ExtensionPluginInfo animExtensionPluginInfo = {
     .nEffects		= NUM_EFFECTS,
